594e8950f101ac7ee49e311b5397faeae88e3897
Sarah079 <102764961+Sarah079@users.noreply.github.com>
2023-03-16 18:19:16 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+using Architecture.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddCors(options => options.AddDefaultPolicy(
+                include =>
+                {
+                    include.AllowAnyHeader();
+                    include.AllowAnyMethod();
+                    include.AllowAnyOrigin();
+                }));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<ICourseRepository, CourseRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors();
+app.UseAuthorization();
+app.UseAuthentication();
+
+app.MapControllers();
+
+app.Run();
---
+++ ViewModel/CourseViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Architecture.ViewModel
+{
+    public class CourseViewModel
+    {
+        public string Name { get; set; }
+        public string Duration { get; set; }
+        public string Description { get; set; }
+        public int LocationId { get; set; }
+    }
+}
+
+//Viewmodels used to combine two models
+//only for data that you need in the view 
+//sends the data to the view 
